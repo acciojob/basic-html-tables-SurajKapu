54b859ad516a4c87a64abba281d904061aa8fb5f
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2024-03-22 12:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your code here
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+	<!-- the entire body must be written by student -->
+    <script type="text/javascript" src="./script.js">
+</script>
+
+</body>
+
+</html>
